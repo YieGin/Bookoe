b326e2a05896df0803bf94d2d9732faede788e67
YieGin <pumasami7@gmail.com>
2024-02-14 13:56:49 +0100

    fixed venv, added not found page

--- full-auth/app/not-found.tsx
+++ full-auth/app/not-found.tsx
@@ -4,13 +4,13 @@ import { FaHome } from 'react-icons/fa';
 
 const NotFoundPage = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center text-gray-800">
-      <h1 className="text-white text-[32vw] mix-blend-difference slashed-zero font-bold font-neuemontrealcondensed leading-none">404</h1>
-      <p className="text-gray-200 font-neuemontreal text-4xl max-w-[30ch] text-center">
+    <div className="h-screen flex flex-col justify-center items-center text-gray-800 dark:bg-[#11161b]">
+      <h1 className="dark:text-white text-[22vw] mix-blend-difference slashed-zero font-bold font-neuemontrealcondensed leading-none">404</h1>
+      <p className="dark:text-gray-200 font-neuemontreal text-4xl max-w-[30ch] text-center">
         Oops! The page you&apos;re looking for isn&apos;t here.
       </p>
       <Link href="/">
-        <p className="flex items-center justify-center px-6 py-3 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-full transition duration-300">
+        <p className="flex items-center justify-center px-6 py-3 dark:text-white font-bold rounded-full transition duration-300">
           <FaHome className="mr-2" /> Go Home
         </p>
       </Link>
