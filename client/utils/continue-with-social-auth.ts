83e6de7ed221f41894b5888343c61f89b67ea3a5
YieGin <pumasami7@gmail.com>
2024-02-14 16:55:10 +0100

    test

--- client/utils/continue-with-social-auth.ts
+++ client/utils/continue-with-social-auth.ts
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 export default async function continueWithSocialAuth(provider: string, redirect: string) {
     try {
         const url = `${process.env.NEXT_PUBLIC_HOST}/api/o/${provider}/?redirect_uri=${
-            process.env.NEXT_PUBLIC_REDIRECT_URL || 'http://localhost:3000'
+            process.env.NEXT_PUBLIC_REDIRECT_URL || 'https://bookoe-jade.vercel.app'
         }/auth/${redirect}`;
                
         const res = await fetch(url, {
